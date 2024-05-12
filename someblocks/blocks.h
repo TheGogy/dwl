cee06aeb6df6e8861d96dedda5877a7ad15cadaa
George Rawlinson <thegogy@proton.me>
2024-05-12 12:26:04 +0100

    volume does not update unless called

--- someblocks/blocks.h
+++ someblocks/blocks.h
@@ -5,7 +5,7 @@ static const Block blocks[] = {
 	{ "  ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }  '",     30,  0  },
   { "  ", "cut -d' ' -f1 /proc/loadavg",                      30,  0  },
   { "  ", "sensors | awk '/^Core 0/ {print $3 }' | cut -c2-", 30,  0  },
-  { "  ", "pamixer --get-volume-human",                       30,  1  },
+  { "  ", "pamixer --get-volume-human",                       0,   1  },
 	{ "  ", "iwgetid -r",                                       120, 0  },
 	// { "  ", "sb-network",                                       120, 0  },
   { "  ", "battery_status",                                   60,  0  },
