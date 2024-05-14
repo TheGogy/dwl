9e426944002269782be3430a31108825c81b736d
Gogy <thegogy@proton.me>
2024-05-14 03:25:50 +0100

    added logout menu

--- someblocks/blocks.h
+++ someblocks/blocks.h
@@ -2,14 +2,14 @@
 static const Block blocks[] = {
 	/*Icon*/  /*Command*/           /*Update Interval*/   /*Update Signal*/
 	// { "", "curl -s -m 20 'wttr.in/?format=1'",                   120, 0  },
-	{ "  ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }  '",     30,  0  },
-  { "  ", "cut -d' ' -f1 /proc/loadavg",                      30,  0  },
-  { "  ", "sensors | awk '/^Core 0/ {print $3 }' | cut -c2-", 30,  0  },
-  { "  ", "pamixer --get-volume-human",                       30,  1  },
-	{ "  ", "iwgetid -r",                                       120, 0  },
-	// { "  ", "sb-network",                                       120, 0  },
-  { "  ", "battery_status",                                   60,  0  },
-	{ "  ", "date '+%H:%M '",                                   60,  0  },
+	{ "  ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }  '",     30,    0  },
+  { "  ", "cut -d' ' -f1 /proc/loadavg",                      30,    0  },
+  { "  ", "sensors | awk '/^Core 0/ {print $3 }' | cut -c2-", 30,    0  },
+  { "  ", "pamixer --get-volume-human",                       120,   1  },
+	{ "  ", "iwgetid -r",                                       120,   0  },
+	{ "  ", "sb-network",                                       120,   0  },
+  { "  ", "battery_status",                                   120,   0  },
+	{ "  ", "date '+%H:%M '",                                   60,    0  },
 	
 	/* Updates whenever "pkill -SIGRTMIN+10 someblocks" is ran */
 	/* {"", "date '+%b %d (%a) %I:%M%p'", 0, 10}, */
