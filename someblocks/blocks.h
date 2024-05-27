cddb0e1ad6a1db28f4305002e3c79818c39a5282
Gogy <thegogy@proton.me>
2024-05-27 12:27:27 +0100

    set max length for someblocks

--- someblocks/blocks.h
+++ someblocks/blocks.h
@@ -2,15 +2,15 @@
 static const Block blocks[] = {
 	/*Icon*/  /*Command*/           /*Update Interval*/   /*Update Signal*/
 	// { "", "curl -s -m 20 'wttr.in/?format=1'",                   120, 0  },
-	{ " 󰎆  ", "playerctl metadata -f '{{artist}} 「{{title}}」'", 10,    0  },
-	{ "   ", "free -h | awk '/^Mem/ { print $3\"/\"$2 } '",      30,    0  },
-  { "   ", "cut -d' ' -f1 /proc/loadavg",                      30,    0  },
-  { "   ", "sensors | awk '/^Core 0/ {print $3 }' | cut -c2-", 30,    0  },
-  { "   ", "pamixer --get-volume-human",                       0,     1  },
-  { "   ", "pamixer --default-source --get-volume-human",      0,     2  },
-	{ "   ", "iwgetid -r",                                       120,   0  },
-  { " ",    "battery_status",                                   300,   0  },
-	{ "   ", "date '+%H:%M '",                                   60,    0  },
+	{ " 󰎆  ", "playerctl metadata -f '{{trunc(artist,20)}} 「{{trunc(title,20)}}」'", 10,    0  },
+	{ "   ", "free -h | awk '/^Mem/ { print $3\"/\"$2 } '",                          60,    0  },
+  { "   ", "cut -d' ' -f1 /proc/loadavg",                                          60,    0  },
+  { "   ", "sensors | awk '/^Core 0/ {print $3 }' | cut -c2-",                     60,    0  },
+  { "   ", "pamixer --get-volume-human",                                           0,     1  },
+  { "   ", "pamixer --default-source --get-volume-human",                          0,     2  },
+	{ "   ", "iwgetid -r",                                                           120,   0  },
+  { " ",    "battery_status",                                                       300,   0  },
+	{ "   ", "date '+%H:%M '",                                                       60,    0  },
 	
 	/* Updates whenever "pkill -SIGRTMIN+10 someblocks" is ran */
 	/* {"", "date '+%b %d (%a) %I:%M%p'", 0, 10}, */
