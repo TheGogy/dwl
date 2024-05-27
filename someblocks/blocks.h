17c5eb3d392e62fb0523ea2e001acc1af3048bc9
Gogy <thegogy@proton.me>
2024-05-27 02:00:43 +0100

    added microphone volume

--- someblocks/blocks.h
+++ someblocks/blocks.h
@@ -2,11 +2,12 @@
 static const Block blocks[] = {
 	/*Icon*/  /*Command*/           /*Update Interval*/   /*Update Signal*/
 	// { "", "curl -s -m 20 'wttr.in/?format=1'",                   120, 0  },
-	{ "   ", "playerctl metadata -f '{{artist}} 「{{title}}」'",   10,    0  },
+	{ " 󰎆  ", "playerctl metadata -f '{{artist}} 「{{title}}」'", 10,    0  },
 	{ "   ", "free -h | awk '/^Mem/ { print $3\"/\"$2 } '",      30,    0  },
   { "   ", "cut -d' ' -f1 /proc/loadavg",                      30,    0  },
   { "   ", "sensors | awk '/^Core 0/ {print $3 }' | cut -c2-", 30,    0  },
   { "   ", "pamixer --get-volume-human",                       0,     1  },
+  { "   ", "pamixer --default-source --get-volume-human",      0,     2  },
 	{ "   ", "iwgetid -r",                                       120,   0  },
   { " ",    "battery_status",                                   300,   0  },
 	{ "   ", "date '+%H:%M '",                                   60,    0  },
